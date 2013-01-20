c97ff9ecee7ba999bd2c65dcb7e8d7001c981b49
Carl Christensen <carl.christensen@bekk.no>
2013-01-20 10:26:26 +0100

    steps

---
+++ app/controllers/Admin.java
@@ -0,0 +1,11 @@
+package controllers;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: carl c. christensen
+ * Date: 11/24/11
+ * Time: 10:00 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Admin {
+}
---
+++ app/controllers/Parsers.java
@@ -0,0 +1,11 @@
+package controllers;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: carl c. christensen
+ * Date: 1/19/13
+ * Time: 2:39 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Parsers {
+}
